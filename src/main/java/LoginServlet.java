c463f3c9d97d54042534837e6af52a96aef7134c
josefmarcc <56867995+josefmarcc@users.noreply.github.com>
2020-03-01 20:29:59 +0100

    kun en bruger adgang

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -7,7 +7,9 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 @WebServlet(name = "LoginServlet", urlPatterns = {"/LoginServlet"})
 public class LoginServlet extends HttpServlet {
@@ -38,6 +40,17 @@ public class LoginServlet extends HttpServlet {
 
         }
 
+        if( ( (Set<String>) servletContext.getAttribute("aktiveBrugere")) == null ){
+
+            Set<String> aktiveBrugere = new HashSet<>();
+            servletContext.setAttribute("aktiveBrugere", aktiveBrugere);
+
+
+        }
+
+
+
+
 
             // hvis brugernavnet ikke findes
         if (!((Map<String, String>) servletContext.getAttribute("brugerMap")).containsKey(navn)) {
@@ -58,12 +71,18 @@ public class LoginServlet extends HttpServlet {
                 request.getRequestDispatcher("WEB-INF/admin.jsp").forward(request,response);
             }
 
-            session.setAttribute("besked", "du logget ind med brugernavnet #" + navn);
-            request.getRequestDispatcher("WEB-INF/HuskeListe.jsp").forward(request, response);
+            if( !( (Set<String>) servletContext.getAttribute("aktiveBrugere")).contains(navn)){
+
+                ((Set<String>) servletContext.getAttribute("aktiveBrugere")).add(navn);
+
+                session.setAttribute("besked", "du logget ind med brugernavnet #" + navn);
+                request.getRequestDispatcher("WEB-INF/HuskeListe.jsp").forward(request, response);
+
+            }
 
         }
         // hvis koden er forkert
-            request.setAttribute("besked", "din kode var forkert, prøv igen");
+            request.setAttribute("besked", "Der gik et eller andet galt, prøv igen");
             request.getRequestDispatcher("index.jsp").forward(request, response);
 
 
